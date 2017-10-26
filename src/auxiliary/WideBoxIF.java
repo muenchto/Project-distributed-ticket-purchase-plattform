ceacfc2abc032b1308249ebbae4c6effd11da7f4
Tobias Münch <tobi.muench@gmail.com>
2017-10-26 17:22:06 +0100

    added argument theaterName

--- src/auxiliary/WideBoxIF.java
+++ src/auxiliary/WideBoxIF.java
@@ -9,7 +9,7 @@ import java.rmi.RemoteException;
 public interface WideBoxIF extends Remote{
     String[] getNames() throws RemoteException;
     Message query(String theaterName) throws RemoteException;
-    Message reserve(Seat old_seat, Seat new_seat, int clientID) throws RemoteException;
-    Message accept(Seat seat, int clientID) throws RemoteException;
-    Message cancel(Seat seat, int clientID) throws RemoteException;
+    Message reserve(String theaterName, Seat old_seat, Seat new_seat, int clientID) throws RemoteException;
+    Message accept(String theaterName, Seat seat, int clientID) throws RemoteException;
+    Message cancel(String theaterName, Seat seat, int clientID) throws RemoteException;
 }
