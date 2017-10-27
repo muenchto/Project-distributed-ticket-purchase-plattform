a71bd73bd1ab7a8805749683d75c043a5e5c3736
hugoamieira <hugoamieira@10.21.230.16>
2017-10-27 13:48:39 +0100

    DBServer added

--- src/auxiliary/DataStorageIF.java
+++ src/auxiliary/DataStorageIF.java
@@ -1,15 +1,37 @@
 package auxiliary;
 
-import java.rmi.Remote;
+
 import java.rmi.RemoteException;
+import java.util.TreeSet;
 
 /**
- * PSD Project - Phase 1
- * @author group: psd002 ; members: 42560-50586-30360
+ * Created by tobiasmuench on 24.10.17.
+ * Note to Hugo: I just set this up for test purpose.
+ * I think we need this in order to be able to call the DBServer via RMI.
+ * I will state here the methods that I think the DBServer has to be able to answer very soon!
  */
-public interface DataStorageIF extends Remote {
 
-    String[] getTheaterNames() throws RemoteException;
-    Theater getTheater(String theaterName) throws RemoteException;
+public interface DataStorageIF {
+
+    public String[] getTheaterNames() throws RemoteException;
+    public Theater getTheater(int theaterName) throws RemoteException;
+
+    // make a purchase and return true if the operation was successful
+    public boolean purchase (int theater, Seat seat) throws RemoteException;
+    
+    // cancel a reservation of a seat, return true if the seat was reserved
+    public boolean cancelReserve(int theater, Seat seat) throws RemoteException;
+    
+    // Just in case we need it relative to reservation
+    // make a random reservation within a free seat and return true if the operation was successful
+    public boolean reserveSeat (int theater, Seat seat) throws RemoteException; 
+    
+    // make a reserve within a specific seat and return true if the operation was successful
+    public Seat reserveSeat(int theater) throws RemoteException;
+  
     boolean occupySeat(String theaterName, Seat theaterSeat) throws RemoteException;
-}
+    
+   
+    
+    
+
---
+++ src/dbserver/DBServer.java
@@ -0,0 +1,102 @@
+package dbserver;
+
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+import auxiliary.*;
+import auxiliary.Seat.SeatStatus;
+
+public class DBServer extends UnicastRemoteObject implements DataStorageIF {
+
+	protected DBServer() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7420422297302142902L;
+	final static String DBFILENAME = "dbfile.txt";
+	final static String LOGFILENAME = "logfile.txt";
+	
+	public Theater[] theaters = new Theater[1500];
+	
+	
+	
+	
+	public static void main(String[] args) throws RemoteException {{
+			System.out.println("Starting server");
+			// CORRECT RMI TO SERVE THE METHODES IMPLEMETED HERE
+			//	Registry registry = LocateRegistry.createRegistry(5099);
+			//	registry.rebind("DBServer", DBServer);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+	@Override
+	public String[] getTheaterNames() {
+		String[] names = null;
+		for (int i=0;i<theaters.length;i++) {
+			names[i]=theaters[i].theaterName;
+		}
+		return names;
+	}
+
+	@Override
+	public Theater getTheater(int theaterName) {
+		return theaters[theaterName];
+	}
+
+	@Override
+	public boolean purchase(int theater, Seat seat) {
+		if (theaters[theater].seats[seat.rowNr][seat.colNr].status==SeatStatus.FREE||
+				theaters[theater].seats[seat.rowNr][seat.colNr].status==SeatStatus.RESERVED)
+			theaters[theater].seats[seat.rowNr][seat.colNr].status=SeatStatus.OCCUPIED;
+		else
+			return false;
+		return true;
+	}
+
+	@Override
+	public boolean cancelReserve(int theater, Seat seat) {
+		if (theaters[theater].seats[seat.rowNr][seat.colNr].status==SeatStatus.RESERVED)
+			theaters[theater].seats[seat.rowNr][seat.colNr].status=SeatStatus.FREE;
+		else
+			return false;
+	return true;
+	}
+
+	@Override
+	public boolean reserveSeat(int theater, Seat seat) {
+		if (theaters[theater].seats[seat.rowNr][seat.colNr].status==SeatStatus.FREE)
+			theaters[theater].seats[seat.rowNr][seat.colNr].status=SeatStatus.RESERVED;
+		else
+			return false;
+		return true;
+	}
+
+	@Override
+	public Seat reserveSeat(int theater) {
+		//TODO return a random free seat from the theater
+		return null;
+	}
+
+}
+
