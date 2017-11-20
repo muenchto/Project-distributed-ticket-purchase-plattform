50c6ad662369a19593d209a982051f7e40e931aa
simplept <cn.canina.caan.alex@gmail.com>
2017-11-20 14:33:42 +0000

    basic FG done

--- src/client/FaultGeneratorThread.java
+++ src/client/FaultGeneratorThread.java
@@ -1,13 +1,23 @@
+package client;
 
-public FaultGeneratorThread() extends Thread{
+import auxiliary.DataStorageIF;
+import auxiliary.WideBoxIF;
 
-    String type;
-    String serverId;
+import java.rmi.Remote;
 
-    public FaultGeneratorThread(String type, String serverIdi, Remote stub){
-        this.type = type;
+public class FaultGeneratorThread extends Thread{
+
+    private String serverId;
+    private WideBoxIF wideBoxStub;
+    private DataStorageIF dbServerStub;
+
+    public FaultGeneratorThread(String serverIdi, Remote stub){
         this.serverId = serverId;
-        if(stub.getClass().equals)
+        if(stub.getClass().equals(WideBoxIF.class)){
+            this.wideBoxStub = (WideBoxIF) stub;
+        }else{
+            this.dbServerStub = (DataStorageIF) stub;
+        }
     }
 
     @Override
